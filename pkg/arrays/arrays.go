c4de7f5140bf20e1dce3dc858d7135904c0810e3
oprimogus <gustavo.jesus@proton.me>
2024-01-09 20:03:32 -0300

    test

--- pkg/arrays/arrays.go
+++ pkg/arrays/arrays.go
@@ -1,21 +1,20 @@
 package arrays
 
 // Map is a function like a map of javascript
-// 
 func Map[T any, U any](s []T, f func(T) U) []U {
-    result := make([]U, len(s))
-    for i, v := range s {
-        result[i] = f(v)
-    }
-    return result
+	result := make([]U, len(s))
+	for i, v := range s {
+		result[i] = f(v)
+	}
+	return result
 }
 
 // Contains return true if str exists in array/slice s
 func Contains(s []string, str string) bool {
-    for _, v := range s {
-        if v == str {
-            return true
-        }
-    }
-    return false
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
