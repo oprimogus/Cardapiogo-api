fa6e9be1f6c456ab6dfb38c1a2f772f380354cdf
Gustavo Ferreira <gustavo.jesus@proton.me>
2024-06-12 01:07:27 -0300

    refactor: remove garbage

--- internal/infra/database/sql/mocks/store_restaurant_type.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO "store_restaurant_type" ("id", "restaurant_type")
-VALUES
-  ((SELECT id FROM "store" WHERE "name" = 'The Clucking Bell'), 'AMERICAN'),
-  ((SELECT id FROM "store" WHERE "name" = 'Up-n-Atom Burger'), 'AMERICAN'),
-  ((SELECT id FROM "store" WHERE "name" = 'Bean Machine'), 'ITALIAN'),
-  ((SELECT id FROM "store" WHERE "name" = 'The Clucking Bell'), 'MEXICAN'),
-  ((SELECT id FROM "store" WHERE "name" = 'Bean Machine'), 'AMERICAN');
-
