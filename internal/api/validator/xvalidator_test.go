03c506f53c279912d36bb4f729936be2c927b880
oprimogus <gustavo.jesus@proton.me>
2024-08-25 12:07:37 -0300

    fix: import

--- internal/api/validator/xvalidator_test.go
+++ internal/api/validator/xvalidator_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
-	xvalidator "github.com/oprimogus/cardapiogo/internal/infrastructure/api/validator"
+	xvalidator "github.com/oprimogus/cardapiogo/internal/api/validator"
 )
 
 type ServiceSuite struct {
